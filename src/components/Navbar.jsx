2b8d5481dff9f09253de4febf48b29a9e35e0d84
M00ose <tyrone.jacalan@protonmail.com>
2023-04-26 21:04:18 +0800

    Moved logo to center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,45 @@
-import { motion } from 'framer-motion';
-import { Rock_Salt } from 'next/font/google';
+import { motion } from "framer-motion";
+import { Rock_Salt } from "next/font/google";
 
-import { containerVariants, animateNameVariants , positionNameVariants} from '../utils/motion';
+import {
+  containerVariants,
+  animateNameVariants,
+  positionNameVariants,
+} from "../utils/motion";
 
-const rockSalt = Rock_Salt({ subsets: ['latin'], weight: '400'});
+const rockSalt = Rock_Salt({ subsets: ["latin"], weight: "400" });
 
 const Navbar = () => {
   const firstName = "ANTONIO";
   const lastName = "PASTORIZA";
 
   return (
-    <motion.div 
+    <motion.div
       variants={positionNameVariants}
       initial="before"
       animate="after"
-      className={`${rockSalt.className} flex h-screen max-w-screen`}
+      className={`${rockSalt.className} flex justify-center h-screen max-w-screen`}
       aria-label="logo"
     >
-        <motion.div 
-            variants={containerVariants} 
-            className='p-2 md:p-4'
-        >
-          <div className='text-center'>
-            <div>
-              {firstName.split('').map((letter, index) => (
-              <motion.span key={index} variants={animateNameVariants}>{letter}</motion.span>
-              ))}
-            </div>
-            <div>
-              {lastName.split('').map((letter, index) => (
-              <motion.span key={index} variants={animateNameVariants}>{letter}</motion.span>
-              ))}
-            </div>
-          </div>        
-        </motion.div>
+      <motion.div variants={containerVariants} className="p-2 md:p-4">
+        <div className="text-center">
+          <div>
+            {firstName.split("").map((letter, index) => (
+              <motion.span key={index} variants={animateNameVariants}>
+                {letter}
+              </motion.span>
+            ))}
+          </div>
+          <div>
+            {lastName.split("").map((letter, index) => (
+              <motion.span key={index} variants={animateNameVariants}>
+                {letter}
+              </motion.span>
+            ))}
+          </div>
+        </div>
+      </motion.div>
     </motion.div>
-    
   );
 };
 
