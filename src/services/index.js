c68273e100d4a8717402de326ae271c0e64add0d
M00ose <tyrone.jacalan@protonmail.com>
2023-04-26 21:02:33 +0800

    Initial commit

---
+++ src/services/index.js
@@ -0,0 +1,98 @@
+import { gql, GraphQLClient } from "graphql-request";
+
+require("dotenv").config();
+
+const graphqlAPI = process.env.GRAPHQL_API_KEY;
+const graphcms = new GraphQLClient(graphqlAPI);
+
+export const getPosts = async () => {
+  const query = gql`
+    {
+      posts {
+        id
+        title
+        datePublished
+        slug
+        content {
+          html
+        }
+      }
+    }
+  `;
+
+  return await graphcms.request(query);
+};
+
+export const getPost = async (slug) => {
+  const query = gql`
+    query Post($slug: String!) {
+      post(where: { slug: $slug }) {
+        id
+        title
+        slug
+        datePublished
+        content {
+          html
+        }
+      }
+    }
+  `;
+
+  return await graphcms.request(query, { slug });
+};
+
+export const getSlugList = async () => {
+  const query = gql`
+    {
+      posts {
+        slug
+      }
+    }
+  `;
+
+  return await graphcms.request(query);
+};
+
+export const getGallery = async () => {
+  const query = gql`
+    {
+ 
+    }
+  `;
+
+  return await graphcms.request(query);
+};
+export const getPiece = async (slug) => {
+  const query = gql`
+    query Gallery($slug: String!) {
+      post(where: { slug: $slug }) {
+        name
+        origin
+        coverPhoto {
+          url
+          height
+          width
+        }
+        slug
+        datePublished
+        description {
+          html
+        }
+      }
+    }
+  `;
+
+  return await graphcms.request(query, { slug });
+};
+
+export const getEntrepreneurSlugList = async () => {
+  const query = gql`
+    {
+      entrepreneurs {
+        slug
+      }
+    }
+  `;
+
+  return await graphcms.request(query);
+};
