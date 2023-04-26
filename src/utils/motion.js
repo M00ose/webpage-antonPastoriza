ffc1111f9bdf9a34d1a16a62a6d8d58a51cc4304
M00ose <tyrone.jacalan@protonmail.com>
2023-04-26 21:05:52 +0800

    Removed content alignment classes

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,34 +1,29 @@
 const containerVariants = {
-    before: {},
-    after: { transition: { staggerChildren: 0.05 } }
-  };
-  
+  before: {},
+  after: { transition: { staggerChildren: 0.05 } },
+};
+
 const animateNameVariants = {
-    before: { opacity: 0, y: 20},
-    after: { opacity: 1, y: 0 }
+  before: { opacity: 0, y: 20 },
+  after: { opacity: 1, y: 0 },
 };
 
 const positionNameVariants = {
-    before: { 
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "8rem",
-      "@media (max-width: 640px)": {
-        fontSize: "5rem"},
+  before: {
+    alignItems: "center",
+    fontSize: "8rem",
+    "@media (max-width: 640px)": {
+      fontSize: "5rem",
     },
-    after: {
-      justifyContent: "start",
-      alignItems: "start",
-      fontSize: "0.875rem",
-      transition: {
-        delay: 3,
-        duration: 1,
-      },
+  },
+  after: {
+    alignItems: "start",
+    fontSize: "0.875rem",
+    transition: {
+      delay: 3,
+      duration: 1,
     },
-}
+  },
+};
 
-export {
-  animateNameVariants,
-  containerVariants,
-  positionNameVariants
-}
+export { animateNameVariants, containerVariants, positionNameVariants };
