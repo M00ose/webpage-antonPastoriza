f4fb211dcd3fab190e9577183e20206c22e07a40
M00ose <tyrone.jacalan@protonmail.com>
2023-05-11 14:54:51 +0800

    Moved to the right

---
+++ src/pages/components/Socials.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+
+import { CiFacebook, CiInstagram, CiLinkedin } from "react-icons/ci";
+
+const socials = [
+  {
+    label: "Facebook",
+    url: "https://www.facebook.com/",
+    icon: <CiFacebook />,
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/",
+    icon: <CiInstagram />,
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/",
+    icon: <CiLinkedin />,
+  },
+];
+const Socials = () => {
+  return (
+    <div className="absolute top-10 right-5">
+      <div className="flex flex-col h-fit">
+        {socials.map((social) => (
+          <Link
+            href={social.url}
+            key={social.label}
+            aria-label={social.label}
+            className="text-2xl text-white hover:scale-150 hover:-translate-x-1 hover:my-2 transition-all"
+          >
+            {social.icon}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Socials;
