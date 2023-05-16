75870dbe2a92e2b8d8c81f98a7ab71c90410c3f4
M00ose <tyrone.jacalan@protonmail.com>
2023-05-16 17:09:13 +0800

    Initial commit

---
+++ src/pages/components/ArtworkCard.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export function ArtworkCard(props) {
+  return (
+    <div className="flex flex-row items-start justify-center gap-8 ">
+      <div className="max-h-lg max-w-lg">
+        <Image
+          key={props.data.title}
+          src={props.data.src}
+          alt="Image"
+          height={props.data.height}
+          width={props.data.width}
+        />
+      </div>
+      <div className="max-w-[200px] max-h-[500px] p-2 text-white text-left overflow-y-scroll scrollbar">
+        <div className="font-bold text-xl">{props.data.title}</div>
+        <div className="text-xl">{props.data.dimensions}</div>
+        <div className="font-italic">{props.data.media}</div>
+        <div className="bg-white w-3/4 h-[0.1rem] my-4"></div>
+        <div>{props.data.description}</div>
+      </div>
+    </div>
+  );
+}
