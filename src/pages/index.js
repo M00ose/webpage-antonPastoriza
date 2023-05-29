d4860bca390f555d8a45f64dc51432dc18d8b940
M00ose <tyrone.jacalan@protonmail.com>
2023-05-29 22:51:39 +0800

    Added background gradients; updated structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,47 +51,52 @@ export default function Index({ artworks }) {
     //TODO: Add loading effect
   }
   return (
-    <main className="h-screen w-screen bg-hero-pattern bg-no-repeat bg-cover overflow-hidden">
+    <main className="fixed h-screen w-screen bg-black bg-no-repeat bg-cover overflow-hidden">
       {router.query.image && (
         <Modal onClose={() => router.push("")}>
           <ArtworkCard image={router.query.image} data={active} />
         </Modal>
       )}
-      <div className="flex flex-col items-center justify-between h-full">
+      <div className="flex flex-col h-full z-30">
         <Navbar />
         <Socials />
-        <div className="flex-1 flex flex-row items-center m-20 max-h-[60%] shadow-3xl">
-          {artworks.map((artwork) => (
-            <Link
-              key={artwork.title}
-              href={`/?image=${artwork.image.url}`}
-              className="h-full"
-            >
-              <Image
-                src={artwork.image.url}
-                alt={artwork.title}
-                height={500}
-                width={500}
-                priority={priority}
-                className={`object-cover h-full max-w-${imageWidth}vw hover:scale-110 hover:drop-shadow-lg transition-all`}
-                onClick={() => {
-                  setActive({
-                    dimensions: artwork.dimensions,
-                    description: artwork.description,
-                    height: artwork.image.height,
-                    media: artwork.media,
-                    src: artwork.image.url,
-                    title: artwork.title,
-                    width: artwork.image.width,
-                    year: artwork.year,
-                  });
-                }}
-              />
-            </Link>
-          ))}
+        <div className="flex-1 flex items-center justify-center z-10">
+          <div className="flex flex-row items-center justify-center h-[50%] w-full mx-20 shadow-3xl">
+            {artworks.map((artwork) => (
+              <Link
+                key={artwork.title}
+                href={`/?image=${artwork.image.url}`}
+                className="h-full"
+              >
+                <Image
+                  src={artwork.image.url}
+                  alt={artwork.title}
+                  height={500}
+                  width={500}
+                  priority={priority}
+                  className={`object-cover h-full max-w-${imageWidth}vw hover:scale-110 hover:drop-shadow-lg transition-all`}
+                  onClick={() => {
+                    setActive({
+                      dimensions: artwork.dimensions,
+                      description: artwork.description,
+                      height: artwork.image.height,
+                      media: artwork.media,
+                      src: artwork.image.url,
+                      title: artwork.title,
+                      width: artwork.image.width,
+                      year: artwork.year,
+                    });
+                  }}
+                />
+              </Link>
+            ))}
+          </div>
         </div>
         <Footer />
       </div>
+      <div className="absolute top-0 right-10 h-[800px] w-[800px] rounded-full bg-gradient-to-r from-white via-white to-red-700 blur-3xl opacity-30 z-0"></div>
+      <div className="absolute top-0 left-30 h-[600px] w-[600px] rounded-full bg-blue-300 blur-3xl opacity-30 z-0"></div>
+      <div className="absolute bottom-0 right-0 translate-x-20 h-[400px] w-[400px] rounded-full bg-red-500 blur-3xl opacity-20 z-0"></div>
     </main>
   );
 }
