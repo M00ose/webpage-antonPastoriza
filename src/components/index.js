74634e9e24ec5d55ecb6c5267fc8ae828b998c08
M00ose <tyrone.jacalan@protonmail.com>
2023-06-08 19:12:12 +0800

    Added imports to AboutBody and StaticNavbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,19 @@
+import AboutBody from "./AboutBody";
 import AboutModal from "./AboutModal";
 import ArtworkCard from "./ArtworkCard";
 import Footer from "./Footer";
 import Modal from "./Modal";
 import Navbar from "./Navbar";
 import Socials from "./Socials";
+import StaticNavbar from "./StaticNavbar";
 
-export { AboutModal, ArtworkCard, Footer, Modal, Navbar, Socials };
+export {
+  AboutBody,
+  AboutModal,
+  ArtworkCard,
+  Footer,
+  Modal,
+  Navbar,
+  Socials,
+  StaticNavbar,
+};
