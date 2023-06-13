39f9b3e503d87209d9256dde1852a3e568b826ee
M00ose <tyrone.jacalan@protonmail.com>
2023-06-13 23:30:09 +0800

    Initial commit

---
+++ src/components/AboutImage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
+export default function AboutImage(props) {
+  return (
+    <div className="">
+      <Image
+        key={props.data[props.index].title}
+        src={props.data[props.index].image.url}
+        alt={props.data[props.index].title}
+        height={200}
+        width={200}
+      />
+    </div>
+  );
+}
