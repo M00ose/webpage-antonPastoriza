54f5372f8516acd3110d93fc31a46ec71e988d15
M00ose <tyrone.jacalan@protonmail.com>
2023-06-14 00:02:35 +0800

    Refactored image resizing and container width

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function AboutHero() {
   return (
-    <section className="relative h-full w-full flex flex-row gap-8">
+    <section className="relative h-full w-full min-w-[350px] flex flex-row gap-8">
       <Image
         src="/profile.jpg"
         alt="Antonio Pastoriza"
         height={300}
         width={300}
-        className="h-fit"
+        className="w-[250px] sm:w-fit"
       />
       <div className="flex-1 h-full relative">
         <h2 className="absolute top-56 right-10">Abstract Expressionist</h2>
